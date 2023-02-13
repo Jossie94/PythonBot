bcbbd627081cf4a442b503ef273677e6ca75d13f
Jossie94 <josefine.sodergard@hotmail.com>
2023-02-13 12:56:33 +0100

    test_ping

--- main.py
+++ main.py
@@ -33,4 +33,20 @@ async def on_ready():
         print("Something is wrong with api")
 
 
+@client.command(name="ping")
+async def test_ping(ctx):
+    correct_response = 'Pong!'
+    channel = await client.fetch_channel(channel_id)
+    await channel.send(correct_response)
+    guild = client.get_guild(server_id)
+    for member in guild.members:
+        print(member.name)
+
+    def check(m):
+        return m.content == correct_response and m.author.id == target_id
+
+    response = await client.wait_for('message', check=check)
+    assert (response.content == correct_response)
+
+
 client.run(os.getenv('TOKEN2'))
